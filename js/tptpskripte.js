722c66d53de64e75cff4ca89d6d45fbfa9b3ed45
ismailcickusic <ismail.cickusic@fet.ba>
2026-05-09 17:25:30 +0200

    Dodan Javascript kod za tipku koja vraca na pocetak i za filtriranje usluga

--- js/tptpskripte.js
+++ js/tptpskripte.js
@@ -5,6 +5,7 @@ const services = [
     cijena: "15.00KM",
     duration: "20 - 30min",
     alt: "Muško šišanje",
+    filterOpcija: "musko",
     img: "images/muska.png"
     
   },
@@ -14,6 +15,7 @@ const services = [
     cijena: "20.00KM",
     duration: "30-45 min",
     alt: "Žensko šišanje",
+    filterOpcija: "zensko",
     img: "images/zensko.jpg"
   },
   {
@@ -22,6 +24,7 @@ const services = [
     cijena: "10.00KM",
     duration: "15-35 min",
     alt: "Feniranje kose",
+    filterOpcija: "sve",
     img: "images/feniranje.jpg"
   },
   {
@@ -30,6 +33,7 @@ const services = [
     cijena: "30.00KM",
     duration: "60-90 min",
     alt: "Farbanje kose",
+    filterOpcija: "sve",
     img: "images/farbanje.jpg"
   },
 
@@ -39,6 +43,7 @@ const services = [
     cijena: "25.00KM",
     duration: "45-90 min",
     alt: "Pramenovi",
+    filterOpcija: "zensko",
     img: "images/pramenovi.jpg"
   },
 
@@ -48,6 +53,7 @@ const services = [
     cijena: "40.00KM",
     duration: "45-75 min",
     alt: "Svečane frizure",
+    filterOpcija: "zensko",
     img: "images/svecane.jpg"
   },
      {
@@ -56,6 +62,7 @@ const services = [
     cijena: "25.00KM",
     duration: "20-30 min",
     alt: "Tretman za njegu kose",
+    filterOpcija: "sve",
     img: "images/tretman.jpg"
   },
      {
@@ -64,6 +71,7 @@ const services = [
     cijena: "10.00KM",
     duration: "15-20 min",
     alt: "Muško brijanje i uređivanje brade",
+    filterOpcija: "musko",
     img: "images/brada.jpg"
   },
      {
@@ -72,21 +80,29 @@ const services = [
     cijena: "40.00KM",
     duration: "90-120 min",
     alt: "Minival",
+    filterOpcija: "zensko",
     img: "images/minival.jpg"
   }
 
 
 ];
 
+//ChatGPT je pomogao pri pravljenju sistema za paginaciju i filtriranje
 const brPonuda = 4;
 let brojStr = 1;
+let aktivniFilter = "sve";
 
 function prikaziUsluge() {
   const container = document.getElementById("lista-opcija");
   container.innerHTML = "";
+  let filtrirane = services;
+
+  if (aktivniFilter !== "sve") {
+    filtrirane = services.filter(s => s.filterOpcija === aktivniFilter);
+  }
 
   const pocetak = (brojStr - 1) * brPonuda;
-  const numerisanePonude = services.slice(pocetak, pocetak + brPonuda);
+  const numerisanePonude = filtrirane.slice(pocetak, pocetak + brPonuda);
 
   numerisanePonude.forEach(service => {
     container.innerHTML += `
@@ -111,8 +127,12 @@ function prikaziUsluge() {
 function numeracija() {
   const brojStranice = document.getElementById("brojStranice");
   brojStranice.innerHTML = "";
-
-  const brojacStranica = Math.ceil(services.length / brPonuda);
+  
+  let filtrirane = services;
+  if (aktivniFilter !== "sve") {
+    filtrirane = services.filter(s => s.filterOpcija === aktivniFilter);
+  }
+  const brojacStranica = Math.ceil(filtrirane.length / brPonuda);
 
   for (let i = 1; i <= brojacStranica; i++) {
     const paginacijaBtn = document.createElement("span");
@@ -139,13 +159,50 @@ document.getElementById("nazadBtn").addEventListener("click", () => {
 });
 
 document.getElementById("naprijedBtn").addEventListener("click", () => {
+  if(aktivniFilter!="sve"){
+  if (brojStr < Math.ceil(filtrirane.length / brPonuda)) {
+    brojStr++;
+    prikaziUsluge();
+    numeracija();
+  }
+}
+
+  if(aktivniFilter==="sve"){
   if (brojStr < Math.ceil(services.length / brPonuda)) {
     brojStr++;
     prikaziUsluge();
     numeracija();
   }
+}
 });
-
 
 prikaziUsluge();
 numeracija();
+
+document.querySelectorAll("#filteri button").forEach(filterBtn => {
+  filterBtn.addEventListener("click", () => {
+    aktivniFilter = filterBtn.dataset.filter;
+    brojStr = 1;
+    prikaziUsluge();
+    numeracija();
+  });
+  });
+
+ const vrati = document.getElementById("vrati");
+
+  
+  window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+      vrati.style.display = "block";
+    } else {
+      vrati.style.display = "none";
+    }
+  };
+
+  
+  vrati.onclick = function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
